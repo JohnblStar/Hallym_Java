39ea1231fd45659c63ee49c3e48fc93544ac26e4
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2022-09-15 09:31:51 +0900

    Create ComDemo.java

---
+++ First_Grade/Second_Semester/ComplexProject/ComDemo.java
@@ -0,0 +1,14 @@
+
+public class ComDemo {
+
+	public static void main(String[] args) {
+
+		Complex com1 = new Complex(8,3);
+		Complex com2 = new Complex(5,2);
+		
+		System.out.println(com1.add(com2));
+		System.out.println(com1.sub(com2));
+		
+	}
+
+}
